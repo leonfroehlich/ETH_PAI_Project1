ab28a23c404b96c0e573b276358696790a74daba
Leon Fröhlich <lfroehlich@student.ethz.ch>
2025-10-22 19:24:14 +0200

    Improved Model

--- solution.py
+++ solution.py
@@ -1,14 +1,13 @@
 import os
 import typing
-from sklearn.gaussian_process.kernels import *
 import numpy as np
-from sklearn.gaussian_process import GaussianProcessRegressor
 import matplotlib.pyplot as plt
 from matplotlib import cm
-from scipy.stats import norm
-from sklearn.preprocessing import StandardScaler
+from sklearn.gaussian_process.kernels import *
+from sklearn.gaussian_process import GaussianProcessRegressor
 from sklearn.cluster import MiniBatchKMeans
-from math import isfinite
+from scipy.stats import norm
+from scipy.optimize import bisect
 
 
 # Set `EXTENDED_EVALUATION` to `True` in order to visualize your predictions.
@@ -21,109 +20,41 @@ COST_W_NORMAL = 1.0
 
 
 class Model(object):
-    """
-    Model for this task.
-    You need to implement the train_model and generate_predictions methods
-    without changing their signatures, but are allowed to create additional methods.
-    """
-
     def __init__(self):
-        """
-        Initialize your model here.
-        We already provide a random number generator for reproducibility.
-        """
         self.rng = np.random.default_rng(seed=0)
+        self.max_train_points = 5000  # Set to None to use all training points
 
-        # TODO: Add custom initialization for your model here if necessary
-        self._coord_scaler = StandardScaler(with_mean=True, with_std=True)
-        base = RBF(length_scale=np.ones(3), length_scale_bounds=(1e-2, 1e3)) \
-               + RationalQuadratic(alpha=1.0, length_scale=1.0)
-        kernel = ConstantKernel(1.0, (1e-3, 1e3)) * base + WhiteKernel(noise_level=1.0, noise_level_bounds=(1e-6, 1e2))
+        kernel = Matern(length_scale=0.1, length_scale_bounds=(1e-2, 1e2), nu=1.5)* ConstantKernel(constant_value=1.0, constant_value_bounds=(1e-2, 1e2)) + WhiteKernel(noise_level=1.0, noise_level_bounds=(1e-5, 1e1))
 
-        
         self.gpr = GaussianProcessRegressor(
             kernel = kernel,
-            alpha = 1e-10,
-            normalize_y = True,
             random_state = 0,
-            n_restarts_optimizer=8,
-            copy_X_train=False
+            normalize_y = True,
+            copy_X_train = False,
         )
 
-        # Precompute optimal shift for asymmetric *squared* loss under Normal predictive dist
-        # Solve: c * [W + (1 − W) Φ(c)] = (W − 1) φ(c)
-        self.W = COST_W_UNDERPREDICT / COST_W_NORMAL
-        self.c_star = self._solve_asym_sq_shift(self.W)
+    def d_asym_cost_loss(self, x, mu, sigma):
+        """
+        Derivative of the asymmetric cost loss function.
+        :param x: point at which to evaluate the derivative
+        :param mu: GP posterior mean
+        :param sigma: GP posterior stddev
+        """
+        z = (x - mu) / sigma # normalized 
+        # This is derived from the asymetric cost function. Must find root of following to minimize cost:
+        return COST_W_UNDERPREDICT*z*norm.sf(z) + COST_W_NORMAL*z*norm.cdf(z) + (COST_W_NORMAL - COST_W_UNDERPREDICT)*norm.pdf(z)
 
-        # Subsampling cap for large n; set to None to disable
-        self.max_train_points = 3000
-
-        # Will be set in fit
-        self._X_cols = None
-        self._trained_on_subset_idx = None
-
-    @staticmethod
-    def _g_asym_c(c, W):
-        Phi = norm.cdf(c)
-        phi = norm.pdf(c)
-        A = W + (1.0 - W) * Phi
-        return c * A - (W - 1.0) * phi
-
-    @classmethod
-    def _solve_asym_sq_shift(cls, W: float) -> float:
-        # Handle degenerate case
-        if not isfinite(W) or W <= 1.0:
-            return 0.0
-
-        # Monotone root on [0, c_hi]; pick a biggish upper bound then refine
-        # g(0) = 0*A - (W-1)*phi(0) < 0 ; for large c, g(c) ~ c*W > 0.
-        lo, hi = 0.0, 10.0
-        glo = cls._g_asym_c(lo, W)
-        ghi = cls._g_asym_c(hi, W)
-        # Expand hi if needed
-        while ghi <= 0 and hi < 50:
-            hi *= 1.5
-            ghi = cls._g_asym_c(hi, W)
-
-        # Bisection (robust and fast enough)
-        for _ in range(80):
-            mid = 0.5 * (lo + hi)
-            gm = cls._g_asym_c(mid, W)
-            if gm == 0.0:
-                return mid
-            if gm > 0:
-                hi = mid
-            else:
-                lo = mid
-            if hi - lo < 1e-8:
-                break
-        return 0.5 * (lo + hi)
-    
-    def _build_features(self, coords: np.ndarray, flags: np.ndarray, fit_scaler: bool = False) -> np.ndarray:
-        """Combine coordinates and residential flag into single feature matrix."""
-        coords = np.asarray(coords, dtype=float)
-        flags = flags.astype(float).reshape(-1, 1)
-
-        if fit_scaler:
-            self._coord_scaler.fit(coords)
-        coords_scaled = self._coord_scaler.transform(coords)
-
-        return np.hstack([coords, flags])
-    
-    def _maybe_subsample(self, X: np.ndarray, y: np.ndarray) -> typing.Tuple[np.ndarray, np.ndarray, np.ndarray]:
+    def subsample(self, X, y):
         n = X.shape[0]
         if self.max_train_points is None or n <= self.max_train_points:
             self._trained_on_subset_idx = np.arange(n, dtype=int)
             return X, y, self._trained_on_subset_idx
 
-        # Cluster on spatial coords to keep geographic coverage; include area flag weakly
-        # Weight area flag so clustering is mostly spatial but still respects zones
-        coords_scaled = X[:, :2]
-        area = X[:, 2:3]
-        z = np.hstack([coords_scaled, 0.2 * area])
+        # Cluster on spatial coords to keep geographic coverage, don't consider area flag
+        z = X[:, 0:2]
 
         k = self.max_train_points
-        kmeans = MiniBatchKMeans(n_clusters=k, random_state=0, batch_size=2048, n_init=3)
+        kmeans = MiniBatchKMeans(n_clusters=k, random_state=0, batch_size=2048)
         labels = kmeans.fit_predict(z)
 
         # Pick the training point closest to each centroid
@@ -134,7 +65,7 @@ class Model(object):
         for c in range(k):
             idx = np.flatnonzero(labels == c)
             if idx.size == 0:
-                # rare, but just skip
+                # choose random point if no members assigned to this cluster
                 chosen[c] = self.rng.integers(0, n)
                 continue
             # nearest by Euclidean distance in z-space
@@ -145,9 +76,10 @@ class Model(object):
 
         chosen = np.unique(chosen)
         self._trained_on_subset_idx = chosen
+
         return X[chosen], y[chosen], chosen
 
-    # Don't change the name or the signature of this function
+
     def predict_pollution_concentration(self, test_coordinates: np.ndarray, test_area_flags: np.ndarray) -> typing.Tuple[np.ndarray, np.ndarray, np.ndarray]:
         """
         Predict the pollution concentration for a given set of city_areas.
@@ -158,20 +90,18 @@ class Model(object):
             containing your predictions, the GP posterior mean, and the GP posterior stddev (in that order)
         """
 
-        X_test = self._build_features(test_coordinates, test_area_flags, fit_scaler=False)
+        gp_mean, gp_std = self.gpr.predict(test_coordinates, return_std=True)
 
-        gp_mean, gp_std = self.gpr.predict(X_test, return_std=True)
-
-        # Decision rule: asymmetric squared loss in residential areas
-        predictions = gp_mean.copy()
-        if self.c_star != 0.0:
-            is_res = test_area_flags.astype(bool)
-            # μ + σ c*
-            predictions[is_res] = gp_mean[is_res] + gp_std[is_res] * self.c_star
+        predictions = gp_mean
+        # Adjust predictions in residential area using asymmetric cost function
+        for i in range(test_coordinates.shape[0]):
+            if test_area_flags[i]:
+                # Find root of asymmetric cost derivative
+                predictions[i] = bisect(self.d_asym_cost_loss, gp_mean[i] - 10*gp_std[i], gp_mean[i] + 10*gp_std[i], args=(gp_mean[i], gp_std[i]))
 
         return predictions, gp_mean, gp_std
 
-    # Don't change the name or the signature of this function
+
     def fit_model_on_training_data(self, train_targets: np.ndarray, train_coordinates: np.ndarray, train_area_flags: np.ndarray):
         """
         Fit your model on the given training data.
@@ -179,17 +109,15 @@ class Model(object):
         :param train_targets: Training pollution concentrations as a 1d NumPy float array of shape (NUM_SAMPLES,)
         :param train_area_flags: Binary variable denoting whether the 2D training point is in the residential area (1) or not (0)
         """
+        
+        # Subsample
+        print('Subsampling training data...')
+        train_coordinates, train_targets, _ = self.subsample(train_coordinates, train_targets)
+        print(f'Fitting on {train_coordinates.shape[0]} training points.')
 
-        # TODO: Fit your model here
-        X_full = self._build_features(train_coordinates, train_area_flags, fit_scaler=True)
-        y_full = np.asarray(train_targets, dtype=float)
-
-        X_train, y_train, _ = self._maybe_subsample(X_full, y_full)
-
-        self.gpr.fit(X_train, y_train)
+        self.gpr.fit(train_coordinates, train_targets)
 
 
-# You don't have to change this function
 def calculate_cost(ground_truth: np.ndarray, predictions: np.ndarray, area_flags: np.ndarray) -> float:
     """
     Calculates the cost of a set of predictions.
@@ -205,7 +133,7 @@ def calculate_cost(ground_truth: np.ndarray, predictions: np.ndarray, area_flags
     cost = (ground_truth - predictions) ** 2
     weights = np.ones_like(cost) * COST_W_NORMAL
 
-    # Case i): underprediction
+    # Case: underprediction
     mask = (predictions < ground_truth) & [bool(area_flag) for area_flag in area_flags]
     weights[mask] = COST_W_UNDERPREDICT
 
@@ -213,7 +141,6 @@ def calculate_cost(ground_truth: np.ndarray, predictions: np.ndarray, area_flags
     return np.mean(cost * weights)
 
 
-# You don't have to change this function
 def check_within_circle(coordinate, circle_parameters):
     """
     Checks if a coordinate is inside a circle.
@@ -223,7 +150,7 @@ def check_within_circle(coordinate, circle_parameters):
     """
     return (coordinate[0] - circle_parameters[0])**2 + (coordinate[1] - circle_parameters[1])**2 < circle_parameters[2]**2
 
-# You don't have to change this function 
+
 def identify_city_area_flags(grid_coordinates):
     """
     Determines the city_area index for each coordinate in the visualization grid.
@@ -254,7 +181,49 @@ def identify_city_area_flags(grid_coordinates):
 
     return area_flags
 
-# Don't change the name or the signature of this function
+def plot_data(y, coordinates, area_flags):
+    """
+    Plots the training data.
+    :param y: Pollution concentrations
+    :param coordinates: 2D coordinates
+    :param area_flags: city_area info for every sample in a form of a bool array (NUM_SAMPLES,)
+    """
+    plt.figure()
+    plt.title('Training data visualization')
+    # Separate into two groups based on area_flags
+    in_area_coords, in_area_y = coordinates[area_flags], y[area_flags]
+    out_area_coords, out_area_y = coordinates[~area_flags], y[~area_flags]
+
+    # Circles coordinates
+    circles = np.array([[0.5488135, 0.71518937, 0.17167342],
+                    [0.79915856, 0.46147936, 0.1567626 ],
+                    [0.26455561, 0.77423369, 0.10298338],
+                    [0.6976312,  0.06022547, 0.04015634],
+                    [0.31542835, 0.36371077, 0.17985623],
+                    [0.15896958, 0.11037514, 0.07244247],
+                    [0.82099323, 0.09710128, 0.08136552],
+                    [0.41426299, 0.0641475,  0.04442035],
+                    [0.09394051, 0.5759465,  0.08729856],
+                    [0.84640867, 0.69947928, 0.04568374],
+                    [0.23789282, 0.934214,   0.04039037],
+                    [0.82076712, 0.90884372, 0.07434012],
+                    [0.09961493, 0.94530153, 0.04755969],
+                    [0.88172021, 0.2724369,  0.04483477],
+                    [0.9425836,  0.6339977,  0.04979664]])
+
+    scatter = plt.scatter(in_area_coords[:, 0], in_area_coords[:, 1], c=in_area_y, cmap=cm.viridis, marker='o', label='In City Area', edgecolor='k')
+    scatter = plt.scatter(out_area_coords[:, 0], out_area_coords[:, 1], c=out_area_y, cmap=cm.viridis, marker='s', label='Out of City Area')
+    # Plot city centers
+    # for circle in circles:
+    #     circle_patch = plt.Circle((circle[0], circle[1]), circle[2], color='red')
+    #     plt.gca().add_patch(circle_patch)
+
+    plt.colorbar(scatter, label='Pollution Concentration')
+    plt.xlabel('X Coordinate')
+    plt.ylabel('Y Coordinate')
+    plt.show()
+
+
 def perform_extended_model_evaluation(model: Model, output_dir: str = '/results'):
     """
     Visualizes the predictions of a fitted model.
@@ -291,7 +260,7 @@ def perform_extended_model_evaluation(model: Model, output_dir: str = '/results'
 
     plt.show()
 
-# Don't change the name or the signature of this function
+
 def get_city_area_data(train_x: np.ndarray, test_x: np.ndarray) -> typing.Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
     """
     Extracts the city_area information from the training and test features.
@@ -305,11 +274,10 @@ def get_city_area_data(train_x: np.ndarray, test_x: np.ndarray) -> typing.Tuple[
     test_coordinates = np.zeros((test_x.shape[0], 2), dtype=float)
     test_area_flags = np.zeros((test_x.shape[0],), dtype=bool)
 
-    #TODO: Extract the city_area information from the training and test features
-    train_coordinates = train_x[:, 0:2]
-    train_area_flags = train_x[:, 2].astype(bool)
-    test_coordinates = test_x[:, 0:2]
-    test_area_flags = test_x[:, 2].astype(bool)
+    train_coordinates[:] = train_x[:, 0:2]
+    train_area_flags[:] = train_x[:, 2].astype(bool, copy=False)
+    test_coordinates[:] = test_x[:, 0:2]
+    test_area_flags[:] = test_x[:, 2].astype(bool, copy=False)
 
     assert train_coordinates.shape[0] == train_area_flags.shape[0] and test_coordinates.shape[0] == test_area_flags.shape[0]
     assert train_coordinates.shape[1] == 2 and test_coordinates.shape[1] == 2
@@ -317,7 +285,7 @@ def get_city_area_data(train_x: np.ndarray, test_x: np.ndarray) -> typing.Tuple[
 
     return train_coordinates, train_area_flags, test_coordinates, test_area_flags
 
-# you don't have to change this function
+
 def main():
     # Load the training dateset and test features
     train_x = np.loadtxt('train_x.csv', delimiter=',', skiprows=1)
